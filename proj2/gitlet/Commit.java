4e09c2cc632df3684e173efe99a6e3396ca94b02
Kevin Chen <kevin@Kevinui-MacBookPro.local>
2024-12-16 18:46:53 +1100

    added init command with initial commit

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,7 +2,9 @@ package gitlet;
 
 // TODO: any imports you need here
 
-import java.util.Date; // TODO: You'll likely use this in this class
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
@@ -10,7 +12,7 @@ import java.util.Date; // TODO: You'll likely use this in this class
  *
  *  @author TODO
  */
-public class Commit {
+public class Commit implements Serializable {
     /**
      * TODO: add instance variables here.
      *
@@ -21,6 +23,44 @@ public class Commit {
 
     /** The message of this Commit. */
     private String message;
+    /** The timestamp of this Commit. */
+    private Date timestamp;
+    /** Link to Parent Commit. */
+    private String parent;
+    /** Link to Second Parent Commit for merges. */
+    private String parent1;
+    private HashMap<String, String> trackedFiles;
+    /**
+     * Each commit (rectangle) points to some blobs (circles), which contain file contents
+     * The commits contain the file names and references to these blobs, as well as a parent link.
+     * These references, depicted as arrows, are represented in the .gitlet directory using their SHA-1 hash values
+     * (the small hexadecimal numerals above the commits and below the blobs).
+     * Your commit class will somehow store all of the information that this diagram shows:
+     * a careful selection of internal data structures will make the implementation easier or harder
+     * */
+    /** a mapping of file names to blob references, a parent reference, and (for merges) a second parent reference
+     * every commit in our case–has a unique integer id that serves as a reference to the object
+     * In the case of commits, it means the same metadata, the same mapping of names to references,
+     * and the same parent reference.
+     * Include all metadata and references when hashing a commit.
+     * Distinguishing somehow between hashes for commits and hashes for blobs.
+     * A good way of doing this involves a well-thought out directory structure within the .gitlet directory.
+     * Another way to do so is to hash in an extra word for each object that has one value for blobs
+     * and another for commits.
+     * */
 
-    /* TODO: fill in the rest of this class. */
+
+    public Commit(String message, String parent) {
+        this.message = message;
+        this.parent = parent;
+        this.parent1 = null;
+        if (parent == null) {
+            this.timestamp = new Date(0);
+        } else {
+            this.timestamp = new Date();
+        }
+        this.trackedFiles = new HashMap<String, String>();
+    }
+
+
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -25,5 +25,30 @@ public class Repository {
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
-    /* TODO: fill in the rest of this class. */
+    /*
+    * TODO: create initial commit
+    *  Initial message: initial commit
+    *  Timestamp for initial commit: 00:00:00 UTC, Thursday, 1 January 1970
+    * TODO: single branch -> intialize master branch which initially points to this initial commit
+    *  and master will be the current branch - current branch?
+    *  What is UID?
+    *   If there is already a Gitlet version-control system in the current directory, it should abort.
+    *   It should NOT overwrite the existing system with a new one. Should print the error message
+    *   A Gitlet version-control system already exists in the current directory.
+    * */
+    public static void init() {
+        if (GITLET_DIR.exists()) {
+            System.out.println("A Gitlet version-control system already exists in the current directory.");
+            System.exit(0);
+        }
+        GITLET_DIR.mkdir();
+        File commits = join(GITLET_DIR, "commits");
+        Commit initialCommit = new Commit("initial commit", null);
+        Utils.writeObject(commits, initialCommit);
+
+
+
+    }
+
+
 }
